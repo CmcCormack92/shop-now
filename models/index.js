53514f87763f511b42a6856f9848588fe9c5c577
CmcCormack92 <86354138+CmcCormack92@users.noreply.github.com>
2022-01-21 21:19:48 -0500

    models

--- models/index.js
+++ models/index.js
@@ -5,10 +5,27 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 
 // Categories have many Products
 
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
+
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'voted_posts',
+  foreignKey: 'user_id'
+});
+User.belongsToMany(Post, {
+  through: Vote,
+  as: 'voted_posts',
+  foreignKey: 'user_id'
+});
 
 // Tags belongToMany Products (through ProductTag)
 
